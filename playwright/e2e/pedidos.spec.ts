3404a1cbb6c5368cfc62d5fe9034676a12fa8d43
Lucas Felice <lucas.defelice@solvace.com>
2026-04-09 16:06:09 -0300

    subindo o desafio do automatizai, teste 'deve consultar um pedido aprovado' refatorado

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -4,21 +4,24 @@ import { test, expect } from '@playwright/test'
 
 
 test('deve consultar um pedido aprovado', async ({ page }) => {
-
   // Arrange
   await page.goto('http://localhost:5173/')
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
+
   await page.getByRole('link', { name: 'Consultar Pedido' }).click()
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
 
   //act
-  await page.getByTestId('search-order-id').fill('VLO-LQ5OS6')
-  await page.getByTestId('search-order-button').click()
+  await page.getByRole('textbox', { name: 'Número do Pedido' }).fill('VLO-LQ5OS6')
+  await page.getByRole('button', { name: 'Buscar Pedido' }).click();
+
 
   //assert
-  await expect(page.getByTestId('order-result-id')).toBeVisible()
-  await expect(page.getByTestId('order-result-id')).toContainText('VLO-LQ5OS6')
 
-  await expect(page.getByTestId('order-result-status')).toBeVisible()
-  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
+  await expect(page.getByText('Pedido', { exact: true })).toBeVisible({ timeout: 10_000 })
+  await expect(page.getByText('Pedido', { exact: true })).toContainText('Pedido')
+
+  await expect(page.getByText('APROVADO')).toBeVisible({ timeout: 10_000 })
+  await expect(page.getByText('APROVADO')).toContainText('APROVADO')
+
 })
